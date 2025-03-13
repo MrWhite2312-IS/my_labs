cd2230921c6221be91a21acdc4ab4d969cfa55ee
SebastianBlanco <sebastian.blancoquesada@ucr.ac.cr>
2025-03-13 10:34:07 -0600

    Creacion del proyecto y solucion Laboratiorio1_c11085

---
+++ Laboratiorio1_C11085/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Laboratiorio1_C11085.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
