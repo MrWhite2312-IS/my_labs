0bb1a12b2166c6c4b0a9cd188c7b05f8a02946bf
Mrwhite2312-IS <quesada2312@gmail.com>
2025-04-02 14:40:17 -0600

    Creacion de Backend-lab

---
+++ backend-lab/backend-lab/Controllers/PaisesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_lab.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaisesController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hola Mundo";
+        }
+
+    }
+}
