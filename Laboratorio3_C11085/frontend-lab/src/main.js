844543489a3bf2ca0057bbbcae211b615ca75349
Mrwhite2312-IS <quesada2312@gmail.com>
2025-04-22 19:48:34 -0600

    Laboratorio#5: Configuracion de Vue router

--- Laboratorio3_C11085/frontend-lab/src/main.js
+++ Laboratorio3_C11085/frontend-lab/src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import {createRouter, createWebHistory} from "vue-router";
+import ListaPaises from "./components/ListaPaises.vue";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: '/',name: "Home", component: ListaPaises},
+    ]
+});
+
+createApp(App).use(router).mount('#app')
